8a3bb9056a6a46353d6a9cc47cba05e1d6e105a3
Jörg Lichter <info@joergv.de>
2022-04-26 16:07:26 +0200

    adds docs

--- src/docs/Page.stories.ts
+++ src/docs/Page.stories.ts
@@ -16,12 +16,21 @@ export const Primary = () => html`<div>
       <ui-grid container>
         <ui-grid item xs="12" sm="6" md="4">
           <ui-heading variant="h2">Bewässerungsplan</ui-heading>
+          <p>
+            Individualisierbare Bewässerungsplände für alle deine Bedürfnisse.
+          </p>
         </ui-grid>
         <ui-grid item xs="12" sm="6" md="4">
           <ui-heading variant="h2">Temperaturmessung</ui-heading>
+          <p>
+            Mit den Temperatursensoren am smartgarden device weißt du immer, welche Temperatur in deinem Beet herscht.
+          </p>
         </ui-grid>
         <ui-grid item xs="12" sm="6" md="4">
           <ui-heading variant="h2">Bodenfeuchtigkeit</ui-heading>
+          <p>
+            Perfekte Bedingungen für deine Pflanzen erreichst du durch eine optimale Bodenfeuchte.
+          </p>
         </ui-grid>
       </ui-grid>
     <ui-container>
@@ -29,7 +38,32 @@ export const Primary = () => html`<div>
   <ui-section primary>
     <ui-container>
       <ui-heading>Die Funktionen</ui-heading>
-      Foo
+
+      <ui-grid container>
+        <ui-grid item sm="6">
+          Bild
+        </ui-grid>
+        <ui-grid item sm="6">
+          <ui-heading variant="h3">Übersicht</ui-heading>
+          <ul>
+                <li>Erstellung von Bewässerungsplänen</li>
+                <li>Dashboard</li>
+                <li>Echtzeit-Informationen</li>
+              </ul>
+        </ui-grid>
+      </ui-grid>
+
+      <ui-grid container>
+        <ui-grid item sm="6">
+          <ui-heading variant="h3">Smardgarden Device</ui-heading>
+          <p>
+            Das Smartgarden Device macht dein Hoochbeet smart und lässt sich über die smartgarden app bedienen.
+          </p>
+        </ui-grid>
+        <ui-grid item sm="6">
+          Bild
+        </ui-grid>
+      </ui-grid>
     <ui-container>
   </ui-section>
   <ui-section>
